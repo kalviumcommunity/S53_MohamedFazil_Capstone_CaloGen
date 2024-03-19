b5106613850f40f5f5e13385c76238bb630e2cc4
MdFazilGITHUB <mjam7221jmi@gmail.com>
2024-03-19 11:06:42 +0530

    added backend server

---
+++ SERVER/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send(`Server is running`);
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on PORT: ${PORT}`);
+});
